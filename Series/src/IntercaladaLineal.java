9d073127c42df668ddb2e1d8bee0e5bca9835a7c
Gabriel Ibañez <j.gabito.ic@gmail.com>
2021-05-25 11:40:38 -0400

    INF333

--- Series/src/IntercaladaLineal.java
+++ Series/src/IntercaladaLineal.java
@@ -1,9 +1,23 @@
+import java.util.Scanner;
 
 public class IntercaladaLineal {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		Scanner x = new Scanner(System.in);
+		System.out.println("Digite valor de N");
+		int N = x.nextInt();
+		int serie = 1;
+		String salida="1"; 
+		for (int i=1 ; i<=N ; i++)
+		{
+			if ( i%2 != 0) // es impar
+				serie = serie + 2;
+			else // es impar
+				serie = serie * 2;
+			//concatenar valores
+			salida = salida + " " + serie;
+		}
+		System.out.print(salida);
 	}
 
 }
