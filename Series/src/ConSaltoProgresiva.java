13cc7aeca27553c435a326dcb15ad3f5918715cb
gabrielito10 <j.gabito@gmail.com>
2021-10-26 11:13:19 -0400

    INF333

---
+++ Series/src/ConSaltoProgresiva.java
@@ -0,0 +1,27 @@
+import java.util.Scanner;
+
+public class ConSaltoProgresiva {
+
+	public static void main(String[] args) {
+		Scanner x = new Scanner(System.in);
+        System.out.print("Digite valor de N -> ");
+        int N = x.nextInt();
+        int Serie1 = 1;
+        int Serie2 = 2;
+        for (int i=1 ; i<=N + 2  ; i++ )
+        {
+        	if (i%2 != 0) // es impar
+        	{
+        		System.out.println(Serie1);
+        		Serie1+=i;
+           	}
+        	else // par
+        	{
+        		System.out.println(Serie2);
+        		Serie2*=i;
+        	}
+        }
+
+	}
+
+}
