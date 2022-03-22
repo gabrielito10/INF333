76704ca682e4a435dd810aedbea3f41a2fff4d76
gabrielito10 <j.gabito@gmail.com>
2022-03-22 10:33:35 -0400

    INF333

--- ClasificacionInformacion/src/MetodoBurbuja.java
+++ ClasificacionInformacion/src/MetodoBurbuja.java
@@ -19,11 +19,11 @@ public class MetodoBurbuja {
 		{
 			for ( int j=0 ; j<N-1 ; j++)
 			{
-				if (array[i] < array[j])
+				if (array[j] > array[j+1])
 				{
-					int aux=array[j];
-					array[j]=array[i];
-					array[i]= aux;
+					int aux=array[j+1];
+					array[j+1]=array[j];
+					array[j]= aux;
 				}
 			}
 		}
--- Series/src/SerieTripleProgresiva.java
+++ Series/src/SerieTripleProgresiva.java
@@ -17,5 +17,4 @@ public class SerieTripleProgresiva {
         	b*=i;
         }
 	}
-
 }
